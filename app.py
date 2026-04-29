edf6cfce35d6f40806cefc83967ff8e755867bae
kentarotaro <kentarotarochan@gmail.com>
2026-04-29 20:16:22 +0700

    refactor: update  mount Gradio app onto FastAPI to enable API endpoints

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import gradio as gr
+import uvicorn
 from app.main import app as fastapi_app
 from app.gradio_ui import demo
 
-# Menempelkan antarmuka Gradio ke root ("/") dari aplikasi FastAPI
+# Mounting Gradio inside FastAPI
 app = gr.mount_gradio_app(fastapi_app, demo, path="/")
+
+if __name__ == "__main__":
+    # Hugging Face menggunakan port 7860 secara default
+    uvicorn.run(app, host="0.0.0.0", port=7860)
